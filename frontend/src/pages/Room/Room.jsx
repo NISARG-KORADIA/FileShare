02df53005d2f52739b67e12f55fc368998bfe7f2
unknown <oo.ks.pp@gmail.com>
2022-04-07 14:09:13 +0530

    roomID

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import {useParams} from 'react-router';
 const Room = () => {
+
+  const {roomID} = useParams();
+
   return (
-    <div>This is your room</div>
+    <div>This is your room - {roomID}</div>
   )
 }
 
